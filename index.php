d781b20dbcc1f650a71d8eb8fef1daa6e75d2632
rhodesit <rhodesit@users.noreply.github.com>
2018-07-05 22:49:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
+<!doctype html>
 <html>
-<script>function php_data(){
-var php_data=<?echo json_encode($_SERVER);?>;
-return php_data;
-}	var data=php_data();
-</script>
-
+   <head>
+      <title>Mirror Bot</title>
+   </head>
    <body>
-      <?php 
-$url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-
-$escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
-echo '<a href="' . $escaped_url . '">' . $escaped_url . '</a>';
-      ?>
+<?php 
+      $url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+      $escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
+      echo '<a href="' . $escaped_url . '">' . $escaped_url . '</a>';
+?>
+   <script>
+   function php_data(){
+      let php_data=<?php echo json_encode($_SERVER);?>;
+      return php_data;
+   }
+   let data=php_data();
+   </script>
    </body>
 </html>
