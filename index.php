3cd660b914847638b0b0ad4b5b8df247937b54a1
dopeslothe <ihaveauthoritah@gmail.com>
2018-07-05 22:33:31 -0400

    .

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <html>
-
+<script>function php_data(){
+var php_data=<?echo json_encode($_SERVER);?>;
+return php_data;
+}	var data=php_data();
+</script>
 
    <body>
-      <?php echo "Hello, World!";?>
+      <?php 
+$url =  "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+$escaped_url = htmlspecialchars( $url, ENT_QUOTES, 'UTF-8' );
+echo '<a href="' . $escaped_url . '">' . $escaped_url . '</a>';
+      ?>
    </body>
 </html>
