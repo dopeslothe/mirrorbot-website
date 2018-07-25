98c54bbc5549a3a5ea715f7a821f31e269fd16f1
Dopeslothe <jah16y@my.fsu.edu>
2018-07-25 09:52:56 -0400

    Added dmca and admin email

--- index.php
+++ index.php
@@ -127,8 +127,7 @@
             }
             echo '<p><a href="' . $data['discord']['url'] . '" target="_blank" rel="noopener">' . $data['discord']['text'] . '</a></p>';
             echo '<p><a href="https://www.reddit.com/r/PublicFreakout" target="_blank" rel="noopener">Back to /r/PublicFreakout</a></p>';
-            echo '\n';
-            echo 'DMCA: dmca@mirrorbot.ga | Anything else: admin@mirrorbot.ga';
+            echo '<p>DMCA: dmca@mirrorbot.ga | Anything else: admin@mirrorbot.ga</p>';
          ?>
       <script>
       function php_data(){
