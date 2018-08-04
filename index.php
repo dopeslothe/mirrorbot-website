aa39ffa309bf381550e0dce1ecd9604b1c26b716
Dopeslothe <jah16y@my.fsu.edu>
2018-08-04 01:56:21 -0400

    spacebar pause/play

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
       return $return;
    }
    function videoplayer($data){
-      return'<video id="videoPlayer" onkeypress="toggleplay(event)" class="video-js vjs-big-play-centered" controls autoplay preload="auto" width="60%" height="35%" poster="'.$data['video']['image_url'].'" data-setup="{}">
+      return'<video id="videoPlayer" class="video-js vjs-big-play-centered" controls autoplay preload="auto" width="60%" height="35%" poster="'.$data['video']['image_url'].'" data-setup="{}">
           <source src="'.$data['video']['url'].'" type="video/mp4">
           <p class="vjs-no-js">
             To view this video please enable JavaScript, and consider upgrading to a web browser that
@@ -141,8 +141,9 @@
       }
       let data=php_data();
       </script>
-      <script>      
-        function toggleplay(event) {
+      <script> 
+      document.getElementById(videoPlayer).onkeypress = function(){toggleplay};
+        function toggleplay() {
             var key = event.which || event.keyCode;
                 var video = document.getElementById("videoPlayer");
                 if (video.player.paused)
